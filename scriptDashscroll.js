0fc5f5ff714e41c9c0b90a8a2a19a397ba496b2d
HarshRaj29004 <142659829+HarshRaj29004@users.noreply.github.com>
2024-02-16 16:00:35 +0530

    drop down feature

--- scriptDashscroll.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('menucontainer').addEventListener('wheel', function(event) {
-    var scrollTop = this.scrollTop;
-    this.scrollTop = scrollTop + event.deltaY;
-    event.preventDefault();
-});
---
+++ scriptdropdown.js
@@ -0,0 +1,5 @@
+let sub_menu = document.getElementById("subMenu");
+
+function togglemenu(){
+    sub_menu.classList.toggle("open-menu");
+}
